4021f4a54afd575f46515bddd2f0a58d011aa83e
alexvanholland <alexvanholland@gmail.com>
2024-11-20 15:15:56 +0100

    berichtje

---
+++ paginas/index.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Scientific Research Article</title>
+</head>
+<body>
+<aside>
+    <ul>
+        <li>1. Abstract</li>
+        <li>2. Inleiding</li>
+        <li>3. Methodologie</li>
+        <li>
+            4. Resultaten
+            <ul>
+                <li>Wiskundig probleemoplossen</li>
+                <li>Kritisch denken</li>
+                <li>Zelfmanagement</li>
+                <li>Academische vaardigheden</li>
+            </ul>
+        </li>
+        <li>5. Conclusie</li>
+</aside>
+
+<h1>Learning to code or coding to learn? A systematic review</h1>
+</body>
+
+
+
+
+
+</html>
